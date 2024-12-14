10a8be4812f803eda62a6c2f4585433753656f39
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-12-14 15:55:14 +0100

    [docker-image] Push versioned build-essential tags
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- src/push_docker_image.sh
+++ src/push_docker_image.sh
@@ -24,6 +24,14 @@ info "Pushing ${IMAGE}"
 ARCH=$(echo "${IMAGE}" | cut -d"/" -f1)
 TARGET_TAG=quay.io/${IMAGE/"${ARCH}/"/""}-"${ARCH}"
 
+# Handle versioned images
+if [ "${2}" == "versioned" ]; then
+	version="$(docker run --rm -a stdout --entrypoint cat ${IMAGE} /etc/droidian-release)"
+	[ -n "${version}" ] || error "Unable to determine image version"
+
+	TARGET_TAG=$(echo "${TARGET_TAG}" | cut -d":" -f1):"${version}-${ARCH}"
+fi
+
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin quay.io
 docker tag "${IMAGE}" "${TARGET_TAG}"
 docker push "${TARGET_TAG}"
